5ba3953dd4c96f0075ca8ab2581f9bb763350528
Fredrik Lönnegren <fredrik.lonnegren@suse.com>
2024-02-28 09:44:00 +0100

    Update to k3s 1.27.10
    Signed-off-by: Fredrik Lönnegren <fredrik.lonnegren@suse.com>

--- install-k3s.sh
+++ install-k3s.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # installs k3s locally:
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.26.7+k3s1" sh -s - server --cluster-init --write-kubeconfig-mode=644
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.27.10+k3s2" sh -s - server --cluster-init --write-kubeconfig-mode=644
